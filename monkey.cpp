c8f12c53add284fabcd4dc4e66da1d3b9f9274e9
RaymundoOV <109765321+RaymundoOV@users.noreply.github.com>
2022-07-21 15:43:14 -0600

    Add files via upload

---
+++ monkey.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void solvef ()
+{
+    int n;
+    cin>>n;
+    vector <int> arr;
+    while (n--)
+    {
+        cin>>arr[n];
+    }
+      while (n--)
+    {
+        cout<<arr[n];
+    } 
+}
+int main ()
+{
+    int T;
+	cin>>T;
+    while (T--)
+    {
+        solvef;
+    
+    }
+  
+    
+    return 0;
+}
